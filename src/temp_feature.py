8bef83173f2e03537854049097ec677922e945e8
yashas7d <138746367+yashas7d@users.noreply.github.com>
2026-05-11 17:19:20 +0530

    revert: remove temporary feature file (reverts c983cd7)

--- src/temp_feature.py
+++
@@ -1,6 +0,0 @@
-def experimental_function():
-    """This is a temporary feature that will be reverted."""
-    return "this should not have been committed"
-
-def another_bad_function():
-    print("oops, wrong branch")
