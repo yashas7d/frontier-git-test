6fc35c56b02271fc6a89ebe0c896bb0da2c341c4
yashas7d <138746367+yashas7d@users.noreply.github.com>
2026-05-11 17:34:30 +0530

    feat: add logger module

---
+++ src/logger.py
@@ -0,0 +1,14 @@
+import logging
+import sys
+
+
+def get_logger(name: str, level: int = logging.INFO) -> logging.Logger:
+    logger = logging.getLogger(name)
+    if not logger.handlers:
+        handler = logging.StreamHandler(sys.stdout)
+        handler.setFormatter(logging.Formatter(
+            "%(asctime)s [%(levelname)s] %(name)s: %(message)s"
+        ))
+        logger.addHandler(handler)
+    logger.setLevel(level)
+    return logger
